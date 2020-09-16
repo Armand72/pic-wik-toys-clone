3fc7eaf49bf819a1ad71a805b9c8ea9c1efe0648
Armand72 <armandmeunier@hotmail.fr>
2020-09-16 14:39:45 +0200

    removes commentaries

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -13,16 +13,4 @@ API.defaults.headers["Access-Control-Allow-Headers"] = "*";
 API.defaults.headers["Access-Control-Allow-Methods"] =
   "GET, POST, PUT, DELETE, OPTIONS";
 
-// API.defaults.withCredentials = true;
-
-// const baseURL =
-//   process.env.NODE_ENV === "development" ? "http://localhost:4000/" : "";
-
-// const API = axios.create({
-//   baseURL,
-//   headers: {
-//     Accept: "application/json",
-//     "Content-type": "application/json",
-//   },
-// });
 export default API;
